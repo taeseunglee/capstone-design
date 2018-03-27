ad9904322f608d810d8f3a70575da0c38eb19020
taeseunglee <taeseunglee159@gmail.com>
2018-03-27 23:18:49 +0900

    assignment6

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # capstone-design
+
+## 파일 위치
+assignment6/web6.js : 과제 6을 위한 js code
+assignment6/data.txt : 데이터 저장되는 곳
---
+++ web6.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+Number.prototype.pad = function(size) {
+    var s = String(this);
+    while (s.length < (size || 2)) {s = "0" + s;}
+    return s;
+};
+
+var seq = 0;
+app.get('/log', function(req, res) {
+    console.log("%j", req.query);
+    var date = new Date();
+
+    result = date.getFullYear() + ""
+            + Number(date.getMonth()+1).pad() + ""
+            + Number(date.getDate()).pad() + ","
+            + Number(date.getHours()).pad() + ":"
+            + Number(date.getMinutes()).pad() + ","
+            + Number(req.query.field1).toFixed(2);
+    
+    fs.appendFile("data.txt", result+"\n", function(err) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    res.end();
+    seq++;
+});
+
+app.get('/get', function(req, res){
+//    fs.readFileSync("data.txt").toString().split(String.fromCharCode(10));
+
+    fs.readFile('data.txt', 'utf8', function(err, data) {
+        if (err) throw err;
+        var arr = data.toString().split(String.fromCharCode(10));
+        var i = 0;
+        var result_body = "";
+        for (i = 0; i < arr.length; ++i) {
+            result_body += arr[i]+"<br>";
+        }
+
+        res.send(result_body);
+        res.end();
+    });
+});
+
+app.listen(8000, () => console.log('Example app listening on port 8000!'))
